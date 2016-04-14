b8b6a0eb9a399dc72312f19d210bfc75902cb2d7
Alban Dericbourg <alban@dericbourg.net>
2016-04-14 12:02:48 +0200

    Unfollow after 6 hours

--- src/main/scala/followme/stream/follow.scala
+++ src/main/scala/followme/stream/follow.scala
@@ -5,10 +5,23 @@ import com.typesafe.scalalogging.LazyLogging
 import followme.stream.TwitterBuilder.twitter
 import twitter4j.User
 
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
+
 case class Follow(user: User)
 
+
 class SerialFollower extends Actor with LazyLogging {
+
+  private[SerialFollower] case class Unfollow(user: User)
+
   override def receive: Receive = {
-    case Follow(user) => twitter.createFriendship(user.getId)
+    case Follow(user) =>
+      twitter.createFriendship(user.getId)
+      context.system.scheduler.scheduleOnce(6 hours) {
+        self ! Unfollow(user)
+      }
+    case Unfollow(user) =>
+      twitter.destroyFriendship(user.getId)
   }
 }
